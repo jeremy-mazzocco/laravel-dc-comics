31eee702ffdae80e2cd178edc0cda4f7eef98dc7
Jeremy Mazzocco <68953007+jeremy-mazzocco@users.noreply.github.com>
2023-07-19 09:39:25 +0100

    add show in route

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,11 @@ use App\Http\Controllers\MainController;
 */
 
 Route::get('/', [MainController::class, 'index'])->name('comics.index');
-Route::get('/comics/create', [MainController::class, 'create'])->name('comics.create');
 
-Route::get('/comics/{id}', [MainController::class, 'show'])->name('comics.show');
+Route::get('/comics/create', [MainController::class, 'create'])->name('comics.create');
 Route::post('/comics/store', [MainController::class, 'store'])->name('comics.store');
+
+Route::get('/comics/show/{id}', [MainController::class, 'show'])->name('comics.show');
 
 Route::get('/comics/edit/{id}', [MainController::class, 'edit'])->name('comics.edit');
 Route::put('/comics/update/{id}', [MainController::class, "update"])->name('comics.update');
